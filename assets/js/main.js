29cc4b427a3d7fc16d4325a71e820bbe73c8ee32
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-02-21 15:10:21 +0100

    refresh button erstellt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
 let grid = document.querySelector(".picsum_grid");
+let refreshBtn = document.querySelector("#refresh");
+let number = 1;
 
-fetch("https://picsum.photos/v2/list")
-  .then((response) => response.json())
-  .then((picsumData) => {
-    console.log(picsumData);
+let fetchFunction = () => {
+  fetch(`https://picsum.photos/v2/list?page=${number}&limit=100`)
+    .then((response) => response.json())
+    .then((picsumData) => {
+      let result = picsumData
+        .map((singleImg) => {
+          return `<div> 
+        <img src="${singleImg.download_url}" alt="Picsum Img">
+        <h2>${singleImg.author} </h2>
+        <a href="${singleImg.url}">See more</a>
+        </div>
+        `;
+        })
+        .join("");
+      grid.innerHTML = result;
+    })
+    .catch((error) => console.log("ein Fehler ist aufgetreten", error));
+};
+fetchFunction();
 
-    picsumData.forEach((singleImg) => {
-      //   console.log(singleImg.id);
-      grid.innerHTML += `<div> 
-      <img src="${singleImg.download_url}" alt="Picsum Img">
-      <h2>${singleImg.author} </h2>
-      <a href="${singleImg.url}">See more</a>
-      `;
-    });
-  })
-  .catch((error) => console.log("ein Fehler ist aufgetreten", error));
+refreshBtn.addEventListener("click", () => {
+  number++;
+  fetchFunction();
+});
